28e51b21bc0bcbac074090493ea3e1a22bddb205
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-22 21:57:09 +0100

    added register user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,49 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
 const asyncHandler = require("express-async-handler");
-// const User = require("../models/userModel");
 
 // @desc Register User
 // @route Post /api/users
 // @access public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // check if user exists
+  const userExits = await User.findOne({ email });
+  if (userExits) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Login User and Authenticated user
