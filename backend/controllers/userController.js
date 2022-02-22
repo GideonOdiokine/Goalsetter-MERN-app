15e08d2502beb833ce828be9b5890d7f55c446a4
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-22 18:54:10 +0100

    added userRoute and UserController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+// @desc Post goals
+// @route Post /api/users
+// @access public
+
+const registerUser = (req, res) => {
+  res.json({ message: "Register User" });
+};
+
+module.exports = {
+  registerUser,
+};
