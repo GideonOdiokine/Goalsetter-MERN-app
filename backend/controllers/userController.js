b4996d93564b7deaa84c8a29ace786a34cce546b
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-22 21:06:00 +0100

    added bcrypt and jsonwebtoken ppackages

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,31 @@
-// @desc Post goals
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+// const User = require("../models/userModel");
+
+// @desc Register User
 // @route Post /api/users
 // @access public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.json({ message: "Register User" });
-};
+});
 
+// @desc Login User and Authenticated user
+// @route Post /api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Login User" });
+});
+
+// @desc Login User and Authenticated user
+// @route Post /api/users/me
+// @access public
+const getMe = asyncHandler(async (req, res) => {
+  res.json({ message: " User data display" });
+});
 module.exports = {
   registerUser,
+  loginUser,
+  getMe,
 };
