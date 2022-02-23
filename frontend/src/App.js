bcd1d86da620af8650d5ffe0486f4d7b63b764bd
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-23 23:57:49 +0100

    import pages to app and index.css to index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-const App = () => {
+function App() {
   return (
     <div>
       <h1>App</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
